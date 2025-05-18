e287252e2250c41d0ca1220762faa5b520b03996
webdesignwithyaser <yaserrihami@gmail.com>
2025-05-18 07:41:49 -0400

    Added files

--- src/Components/UseFetch.jsx
+++ src/Components/UseFetch.jsx
@@ -1,5 +1,12 @@
-const UseFetch = (url) => {
-    
+import React, { useEffect, useState } from 'react'
+
+ const useFetch = (url) => {
+    const[data,setData]=useState();
+    useEffect(()=>{
+        fetch(url).then((res)=>res.json())
+        .then((data)=>setData(data))
+    },[])
+  return [data]
 }
 
-export default UseFetch
+export default useFetch
